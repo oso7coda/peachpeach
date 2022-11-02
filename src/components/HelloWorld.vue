908d19e5bceeca61995e025b93a5ff5871b157b7
oso7coda <oso7coda@gmail.com>
2022-11-02 20:40:42 +0900

    ADD - Price getter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,28 +2,19 @@
   <div class="hello">
     <button v-on:click="getPeachPriceBtn">데이터 가져오기?</button>
      복숭아 가격을 조사해 보자..
-  <div>
-    여기 표도 넣고
-  </div>
-  <div>
+    <div>
+      여기 표도 넣고
+    </div>
+    <div>
 
-    크롤링도 하고..
-  </div>
+      크롤링도 하고..
+    </div>
   </div>
 </template>
 
 <script>
-const axios = require('axios');
-const cheerio = require('cheerio');
-const log = console.log;
+import axios from 'axios'
 
-const getHtml = async () => {
-  try {
-    return await axios.get('https://www.busan.go.kr/market/mktdistributioninfo010102?curPage=1');
-  } catch (error) {
-    console.error(error);
-  }
-};
 
 export default {
   name: 'HelloWorld',
@@ -32,17 +23,7 @@ export default {
   },
   methods: {
     getPeachPriceBtn(){
-        getHtml().then((html) => {
-        // axios 응답 스키마 `data`는 서버가 제공한 응답(데이터)을 받는다.
-        // load()는 인자로 html 문자열을 받아 cheerio 객체 반환
-        const $ = cheerio.load(html.data);
-        console.log($)
-        const data = {
-          mainContents: $('table.boardList > tbody').text(),
-        };
-        return data;
-      })
-      .then((res) => log(res));
+      axios.post('check');
     },
 
   }
