2e08f33de1c0d5a57d475167628660a05aefebd6
oso7coda <oso7coda@gmail.com>
2022-11-02 20:40:11 +0900

    add - config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: './docs',
   publicPath: '/peachpeach',
+
+  pluginOptions: {
+    express: {
+      shouldServeApp: true,
+      serverDir: './srv'
+    }
+  }
 })
